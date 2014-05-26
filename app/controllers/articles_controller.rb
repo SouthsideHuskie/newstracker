36e8561e09503400281b389fe409646685147d86
SouthsideHuskie <r-mcguinness@kellogg.northwestern.edu>
2014-05-26 16:26:49 -0500

    Working

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,74 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+
+    @article.title = params[:title]
+
+    @article.article_url = params[:article_url]
+
+    @article.tag = params[:tag]
+
+    @article.user_id = params[:user_id]
+
+    @article.description = params[:description]
+
+
+
+    if @article.save
+      redirect_to "/articles", :notice => "Article created successfully."
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+
+    @article.title = params[:title]
+
+    @article.article_url = params[:article_url]
+
+    @article.tag = params[:tag]
+
+    @article.user_id = params[:user_id]
+
+    @article.description = params[:description]
+
+
+
+    if @article.save
+      redirect_to "/articles", :notice => "Article updated successfully."
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    @article.destroy
+
+
+    redirect_to "/articles", :notice => "Article deleted."
+
+  end
+end
